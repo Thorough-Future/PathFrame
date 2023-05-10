e319679e22002c88142e7641f3471d3a0f912c2b
S.Wang <wsh@physics.so>
2023-05-10 19:43:48 +0800

    Update ln_model.py

--- project/ln/ln_model.py
+++ project/ln/ln_model.py
@@ -1,7 +1,7 @@
 from model.segmentation import SegModel
 
 class Model(SegModel):
-    """Model class for stomach project."""
+    """Model class for ln project."""
     
     def __init__(self, cfg, data, mode):
         super(Model, self).__init__(cfg, data, mode)
